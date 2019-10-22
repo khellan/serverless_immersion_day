1cd2be1ae5ebf9b299af9c14ff492d55460396fd
Knut Hellan <khe@ambita.com>
2019-10-22 10:48:30 +0200

    Correction of cognito userPoolClientId

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-central-1_AiZAa0PEY', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'arn:aws:cognito-idp:eu-central-1:259177788634:userpool/eu-central-1_AiZAa0PEY', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '42r8sclidnf9h49le70pj99k9s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
