d3862b2e1b95660eb2a1cd59ddb1de02b592dd73
Knut Hellan <khe@ambita.com>
2019-10-22 10:43:40 +0200

    Configure cognito
    Set up user pool information.

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '42r8sclidnf9h49le70pj99k9s', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'arn:aws:cognito-idp:eu-central-1:259177788634:userpool/eu-central-1_AiZAa0PEY', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
